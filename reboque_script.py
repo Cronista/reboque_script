3e2bfd2fe4f4b3e15b5e5491009deb82c92aed3a
Cronista <whamack@live.com>
2024-09-20 00:03:50 -0300

    Update reboque_script.py
    testing HTML parsing

--- reboque_script.py
+++ reboque_script.py
@@ -1,32 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
-url = 'https://books.toscrape.com/catalogue/category/books_1/index.html'
+# url = 'https://books.toscrape.com/catalogue/category/books_1/index.html'
 
-response = requests.get(url)
+# response = requests.get(url)
 
-html_content = response.text
+# html_content = response.text
 
-soup = BeautifulSoup(html_content,'html.parser')
+# soup = BeautifulSoup(html_content,'html.parser')
 
-book_list = soup.find('ul', class_='nav nav-list').find('ul').find_all('li')
+# book_list = soup.find('ul', class_='nav nav-list').find('ul').find_all('li')
 
-for book in book_list:
+# for book in book_list:
     
-    print(book.text.strip())
+#     print(book.text.strip())
 
 # 
 
-url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+# url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
-response = requests.get(url)
+# response = requests.get(url)
 
-html_content = response.text
+# html_content = response.text
 
-soup = BeautifulSoup(html_content,'html.parser')
+# soup = BeautifulSoup(html_content,'html.parser')
 
-book_info = soup.find('article', class_= 'product_page').find('p', class_=None)
+# book_info = soup.find('article', class_= 'product_page').find('p', class_=None)
 
-print(book_info.text.strip())
+# print(book_info.text.strip())
 
+
+#SELENIUM
+
+import selenium
 
