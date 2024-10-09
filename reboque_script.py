7f6b2d2ac448c95c08179072f3c9ec9eef421412
Cronista <whamack@live.com>
2024-10-09 19:01:18 -0300

    Update reboque_script.py

--- reboque_script.py
+++ reboque_script.py
@@ -2,5 +2,5 @@ from helium import *
 from bs4 import BeautifulSoup
 import csv
 
-url = "https://fornecedor.localiza.com/Portal/PortalFornecedor#/login"
+url = "https://fornecedor.localiza.com/Portal/PortalFornecedor#/financeiro/nf-pendentes-envio"
 browser = start_chrome(url, headless=False)
