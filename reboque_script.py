1410489aed353a2d9230a26efd9e48f87e1799f9
Cronista <whamack@live.com>
2024-10-11 15:05:33 -0300

    Update reboque_script.py

--- reboque_script.py
+++ reboque_script.py
@@ -1,7 +1,17 @@
 from helium import *
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import csv
 
 url = "https://fornecedor.localiza.com/Portal/PortalFornecedor#/financeiro/nf-pendentes-envio"
 browser = start_chrome(url, headless=False)
 
+# soup = BeautifulSoup(browser.page_source, 'html.parser')
+
+login_user = S("txt-login-new")
+
+write(env, into=login_user)
+      
+print(login_user.prettify())
+
+browser.quit()
