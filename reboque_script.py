9bba6ea56cf97de2c7d7d4d2b73164a878b655d3
Cronista <whamack@live.com>
2024-10-12 15:01:16 -0300

    Update reboque_script.py

--- reboque_script.py
+++ reboque_script.py
@@ -14,7 +14,7 @@ login_user_localiza = os.environ['LOGIN_USER_LOCALIZA']; login_pass_localiza = o
 url = "https://fornecedor.localiza.com/Portal/PortalFornecedor#/financeiro/nf-pendentes-envio"
 browser = start_chrome(url, headless=False)
 
-# # wait page to load
+#wait page to load
 # time.sleep(1)
 
 #define login field's CSS elements and input credentials
@@ -24,24 +24,20 @@ write(login_user_localiza, into=login_user)
 write(login_pass_localiza, into=login_pass)
 click('Acessar Portal Localiza')
 
+#wait until the jobs table is loaded
 wait_until(S('tbody').exists)
 
+#create a soup element to more easily manipulate the loaded page's HTML elements, compared to pure Helium
 soup = BeautifulSoup(browser.page_source, 'html.parser')
-
 job_table = soup.find('tbody')
 
+#list to store each job dictionary
 job_list = []
  
+#loop to extract jobs
 for job in job_table:
     
-    # index_current = int(protocol.select_one('div:nth-of-type(1)').select_one('span > span').text)
-    
-    # if index_current <= index:
-        
-    #     continue
-    
-    # index += 1
-    
+    #try:except to go over unwanted elements such as empty lines
     try:
         
         job_dict = {
@@ -53,16 +49,7 @@ for job in job_table:
             'Faturamento': job.select_one('td:nth-of-type(7)').text.strip(),
             'Notas Anexadas': job.select_one('td:nth-of-type(8)').text.strip(),
             'Forma de Pagamento': job.select_one('td:nth-of-type(9)').text.strip(),
-            # 'category': protocol.select_one('div:nth-of-type(2)').find('a').text,
-            # 'tvl': protocol.select_one('div:nth-of-type(3)').select_one('span > span').text,
-            # '1d change': protocol.select_one('div:nth-of-type(4)').find('span').text,
-            # '7d change': protocol.select_one('div:nth-of-type(5)').find('span').text,
-            # '1m change': protocol.select_one('div:nth-of-type(6)').find('span').text,
-            # 'fee 24h': protocol.select_one('div:nth-of-type(7)').text,
-            # 'revenue 24h': protocol.select_one('div:nth-of-type(8)').text,
-            # 'spot volume': protocol.select_one('div:nth-of-type(9)').text
-            
-            
+                     
         }
             
             
