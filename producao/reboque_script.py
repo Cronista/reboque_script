6547160219fb4e9aa863f5a79ea83ef2b2c27c13
Cronista <whamack@live.com>
2024-10-16 23:05:25 -0300

    Update reboque_script.py

--- producao/reboque_script.py
+++ producao/reboque_script.py
@@ -1,5 +1,8 @@
 from helium import *
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import csv, os, time
@@ -45,13 +48,13 @@ def jobs_localiza():
 
     #create a soup element to more easily manipulate the loaded page's HTML elements, compared to pure Helium
     soup = BeautifulSoup(browser.page_source, 'html.parser')
-    job_table = soup.find('tbody')
+    job_table_localiza = soup.find('tbody')
 
     #list to store each job dictionary
-    job_list = []
+    job_list_localiza = []
     
     #loop to extract jobs
-    for job in job_table:
+    for job in job_table_localiza:
         
         #try:except to go over unwanted elements such as empty lines
         try:
@@ -69,7 +72,7 @@ def jobs_localiza():
             }
                 
                 
-            job_list.append(job_dict)
+            job_list_localiza.append(job_dict)
             
         except (AttributeError, TypeError) as e:
             continue
@@ -81,7 +84,22 @@ def jobs_localiza():
         
         writer = csv.DictWriter(file, fieldnames=headers)
         writer.writeheader()
-        writer.writerows(job_list)
+        writer.writerows(job_list_localiza)
+    
+    jobs_autem()
+
+    #refresh browser and access cleared jobs with the return value from jobs_pandas
+    #TODO stop webdriver from crashing
+    clear_ss, not_clear_ss = jobs_pandas()
+    refresh()
+    wait_until(S('tbody').exists)
+    
+    for job_cleared in clear_ss:
+        
+        click(job_cleared['ss'])
+        wait_until(S('#NFList > tbody > tr > td:nth-child(6) > div > input').exists)
+        write(job_cleared['faturamento'], into=S('#NFList > tbody > tr > td:nth-child(6) > div > input'))
+         
 
     browser.quit()
     
@@ -130,8 +148,10 @@ def jobs_autem():
     
     #wait until the export button is loaded, delete current file in the directory and click the download (export) button
     #this is done so the file is not renamed to "...(1)". The files will always be copied-over updated.
-    wait_until(S('#datatable_servicos_wrapper > div.dt-buttons > button.dt-button.buttons-excel.buttons-html5.btn-icon-o.btn-light.ti-export.waves-effects.perm-simples').exists)
-   
+    #TODO wait until table is loaded
+    #wait_until(S('#datatable_servicos_wrapper > div.dt-buttons > button.dt-button.buttons-excel.buttons-html5.btn-icon-o.btn-light.ti-export.waves-effects.perm-simples').exists)
+    wait_until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#datatable_servicos_wrapper > div.dt-buttons > button.dt-button.buttons-excel.buttons-html5.btn-icon-o.btn-light.ti-export.waves-effects.perm-simples')))
+    
     if os.path.exists(autem_jobs_file):
         
         os.remove(autem_jobs_file)
@@ -139,60 +159,85 @@ def jobs_autem():
     click(S('#datatable_servicos_wrapper > div.dt-buttons > button.dt-button.buttons-excel.buttons-html5.btn-icon-o.btn-light.ti-export.waves-effects.perm-simples'))
     
     #wait download to finish
-    time.sleep(3)
+    while os.path.exists(autem_jobs_file) == False:
+        
+        time.sleep(1)
+        
     browser.quit()
-    
-# jobs_localiza()    
-# jobs_autem()
 
 ##Pandas
 #Compare job lists
-
-#set up job lists dataframes
-df_localiza = pd.read_csv('producao\jobs_csv\localiza_.csv')
-df_autem = pd.read_excel('producao\jobs_csv\exportGrid_AutEM_xls.xlsx', header= 1)
-
-# #treat Not a Number
-# df_autem = df_autem.replace(np.nan, 'Sem OBS')
-
-#standardize data between dataframes
-#rename df_autem columns to match df_localiza
-df_autem = df_autem.rename(columns= {'Protocolo': 'ss', 'Valor (R$)': 'faturamento', 'Data e Hora Finalizado': 'conclusao', 'CNPJ': 'cnpj_fornecedor', 'Placa': 'placa'})
-
-#make the values from df_localiza 'faturamento' column to match df_autem
-df_localiza['faturamento'] = (df_localiza['faturamento']
-               .str.replace('R\$', '', regex=True)
-               .str.replace('.', '', regex=False) 
-               .str.replace(',', '.', regex=False)
-              )
-
-#convert every non 'Conforme Contrato' text to float, in df_localiza 'faturamento' column
-for index_df_localiza_items, value_df_localiza_items in df_localiza['faturamento'].items():
+def jobs_pandas():
     
-    if value_df_localiza_items != 'Conforme Contrato':
-    
-        df_localiza.at[index_df_localiza_items, 'faturamento'] = pd.to_numeric(value_df_localiza_items)
-    
-    else:
+    #set up job lists dataframes
+    df_localiza = pd.read_csv('producao\jobs_csv\localiza_.csv')
+    df_autem = pd.read_excel('producao\jobs_csv\exportGrid_AutEM_xls.xlsx', header= 1)
+
+    # #treat Not a Number
+    # df_autem = df_autem.replace(np.nan, 'Sem OBS')
+
+    #standardize data between dataframes
+    #rename df_autem columns to match df_localiza
+    df_autem = df_autem.rename(columns= {'Protocolo': 'ss', 'Valor (R$)': 'faturamento', 'Data e Hora Finalizado': 'conclusao', 'CNPJ': 'cnpj_fornecedor', 'Placa': 'placa'})
+
+    #make the values from df_localiza 'faturamento' column to match df_autem
+    df_localiza['faturamento'] = (df_localiza['faturamento']
+                .str.replace('R\$', '', regex=True)
+                .str.replace('.', '', regex=False) 
+                .str.replace(',', '.', regex=False)
+                )
+
+    #convert every non 'Conforme Contrato' text to float, in df_localiza 'faturamento' column
+    for index_df_localiza_items, value_df_localiza_items in df_localiza['faturamento'].items():
         
-        continue
-    
-#drop unused columns
-df_autem = df_autem.drop([
-    'Data e Hora', 'Empresa', 'Produto', 'Documento(s)', 'Checklist / CR / GRV', 'Checklist', 'Tipo de Serviço', 'Veículo / Objeto', 
-    'Cor', 'Chassi', 'Renavam', 'Beneficiário', 'Senha', 'Telefone', 'O. Logradouro', 'O. Bairro','O. Cidade', 'D. Logradouro','D. Bairro', 'D. Cidade', 'Profissional',
-    'Viatura', 'Descrição', 'Tags', 'KM', 'Pedágio (R$)', 'Hora Parada (R$)', 'Hora Trabalhada (R$)', 'OBS', 'Ocorrência(s)', 'cnpj_fornecedor', 'conclusao']
-    , axis=1)
-df_localiza = df_localiza.drop([
-    'notas_anexadas', 'forma_de_pagamento', 'cnpj_fornecedor', 'conclusao']
-     , axis=1)
-    
-#Compare ss to value
-# print(df_localiza['faturamento'].isin(df_autem['faturamento']))
-print(pd.merge(df_localiza, df_autem, on='ss', how='left'))
+        if value_df_localiza_items != 'Conforme Contrato':
+        
+            df_localiza.at[index_df_localiza_items, 'faturamento'] = pd.to_numeric(value_df_localiza_items)
+        
+        else:
+            
+            continue
+        
+    #drop unused columns
+    df_autem = df_autem.drop([
+        'Data e Hora', 'Empresa', 'Produto', 'Documento(s)', 'Checklist / CR / GRV', 'Checklist', 'Tipo de Serviço', 'Veículo / Objeto', 
+        'Cor', 'Chassi', 'Renavam', 'Beneficiário', 'Senha', 'Telefone', 'O. Logradouro', 'O. Bairro','O. Cidade', 'D. Logradouro','D. Bairro', 'D. Cidade', 'Profissional',
+        'Viatura', 'Descrição', 'Tags', 'KM', 'Pedágio (R$)', 'Hora Parada (R$)', 'Hora Trabalhada (R$)', 'OBS', 'Ocorrência(s)', 'cnpj_fornecedor', 'conclusao']
+        , axis=1)
+    df_localiza = df_localiza.drop([
+        'notas_anexadas', 'forma_de_pagamento', 'cnpj_fornecedor', 'conclusao']
+        , axis=1)
 
+    #list of ss cleared and not cleared to continue
+    clear_ss = []
+    not_clear_ss = []
 
+    #dataframe comparing localiza and autem jobs lists
+    merge_localiza_autem = pd.merge(df_localiza, df_autem, on='ss', how='left')
 
-# print(df_localiza.head())
-# print('\n')
-# print(df_autem.head())
+    #Compare ss to value and save the results to two lists of dictionaries
+    for index_merge in range(len(merge_localiza_autem)):
+        
+        if merge_localiza_autem['faturamento_x'][index_merge] == merge_localiza_autem['faturamento_y'][index_merge]:
+            
+            clear_ss_dict = {
+                
+                'ss': merge_localiza_autem['ss'][index_merge],
+                'faturamento': merge_localiza_autem['faturamento_x'][index_merge],
+            }
+            
+            clear_ss.append(clear_ss_dict)
+        else:
+            
+            not_clear_ss_dict = {
+                
+                'ss': merge_localiza_autem['ss'][index_merge],
+                'faturamento': merge_localiza_autem['faturamento_x'][index_merge]
+            }
+            
+            not_clear_ss.append(not_clear_ss_dict)
+            
+    print(clear_ss[0]['ss'])
+    return clear_ss, not_clear_ss
+
+jobs_localiza()
